d188fe27dd964c16a0407a8d297587a8d63a7a8c
dahamrasanga22 <dahamrasanga22@gmail.com>
2026-02-22 01:06:42 +0530

    Login Panel

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LMS</title>
+    
     <!-- Link Boostrap -->
     <link rel="stylesheet" href="CSS\bootstrap\css\bootstrap.min.css">
 </head>
